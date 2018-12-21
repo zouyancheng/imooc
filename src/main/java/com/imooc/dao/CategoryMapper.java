f2960507453e80acf55719d7d6efda5eb4bf548f
zouyancheng <756587420@qq.com>
2018-12-21 12:21:17 +0800

    second commit

---
+++ src/main/java/com/imooc/dao/CategoryMapper.java
@@ -0,0 +1,17 @@
+package com.imooc.dao;
+
+import com.imooc.pojo.Category;
+
+public interface CategoryMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
+}
---
+++ src/main/java/com/imooc/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.imooc.dao;
+
+import com.imooc.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
