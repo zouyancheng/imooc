9cc43cf9c7f9a71fecd998271b3fd146daa2dcb3
zouyancheng <756587420@qq.com>
2019-11-01 12:58:09 +0800

    first commit

---
+++ src/main/java/com/imooc/controller/protal/OrderController.java
@@ -0,0 +1,44 @@
+package com.imooc.controller.protal;
+
+
+import com.imooc.common.Const;
+import com.imooc.common.ServerResponse;
+import com.imooc.enums.ResponseCode;
+import com.imooc.pojo.User;
+import com.imooc.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    IOrderService iOrderService;
+
+
+    @RequestMapping("/create.do")
+    @ResponseBody
+    public ServerResponse create(HttpSession session,Integer shippingId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iOrderService.createOrder(user.getId(),shippingId);
+    }
+
+    @RequestMapping("cancel.do")
+    @ResponseBody
+    public ServerResponse cancel(HttpSession session, Long orderNo){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iOrderService.cancel(user.getId(),orderNo);
+    }
+
+}
